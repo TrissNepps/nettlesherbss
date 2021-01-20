ef8594efe7c351a7df885b3f0679618c937152ca
TrissNepps <74569212+TrissNepps@users.noreply.github.com>
2021-01-20 12:36:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -114,3 +114,30 @@ function updateCounter() {
     .then(data => counterElement.innerHTML = data.value)
 }
 
+//Breath
+
+
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breathAnimation()
+
+function breathAnimation() {
+    text.innerHTML = 'Breath In!'
+    container.className = 'container grow'
+
+    setTimeout(() => {
+        text.innerText = 'Hold'
+
+        setTimeout(() => {
+            text.innerText = 'Breathe Out!'
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breatheTime)
+}
+
+setInterval(breathAnimation, totalTime)
