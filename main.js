64081dff0b6b1769a86435378df767983e8b2bfd
TrissNepps <74569212+TrissNepps@users.noreply.github.com>
2021-01-20 00:11:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -64,18 +64,28 @@ const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-tree'
 
+// Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
+// We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-tree' : 'bx-sun'
 
 
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-tree' ? 'add' : 'remove'](iconTheme)}
 
-themeButton.addEventListener('click', ()=>{
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-
+    // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
@@ -92,3 +102,15 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contact__data, .contact__button, .footer__content', {
     interval: 200
 })
+
+//BELOW IS FOR THE WEBSITE COUNTER
+counterElement = document.getElementsByClassName('count')[0];
+
+updateCounter()
+
+function updateCounter() {
+    fetch('https://api.countapi.xyz/update/nettlesherbs/nettlesherbs/?amount=1')
+    .then(res => res.json())
+    .then(data => counterElement.innerHTML = data.value)
+}
+
